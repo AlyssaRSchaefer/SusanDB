bff261991c5409a1efd54529a029864a4585c387
Alissa Chimienti <alissachimienti@gmail.com>
2025-03-16 16:12:36 -0500

    version with choose mapping key as shown in slides

--- static/js/fields_to_update.js
+++ static/js/fields_to_update.js
@@ -1,6 +1,8 @@
 // Variable to hold the selected fields
 let selectedFields = [];
+let primaryKeys = {}
 
+// STEP 1
 // Function to collect selected fields and switch to the second popup
 function submitFieldsToUpdate() {
     // Collect all the selected checkboxes
@@ -18,6 +20,30 @@ function submitFieldsToUpdate() {
     
     document.getElementById('choose-mapping-key').style.display = 'flex';
 }
+
+// STEP 2
+// Function to handle the submission of selected primary keys (in both tables)
+function submitPrimaryKeys() {
+    // Get selected Excel fields
+    const selectedExcelFields = Array.from(document.querySelectorAll('.excel-fields-checkbox:checked')).map(checkbox => checkbox.value);
+
+    // Get selected SusanDB fields
+    const selectedSusanDBFields = Array.from(document.querySelectorAll('.susandb-fields-checkbox:checked')).map(checkbox => checkbox.value);
+
+    // Store the selected fields in the primaryKeys dictionary
+    primaryKeys = {
+        excelFields: selectedExcelFields,
+        susandbFields: selectedSusanDBFields,
+    };
+
+    console.log(primaryKeys); // Log the primaryKeys dictionary to check the data
+
+    // Optionally, proceed with the next steps after submitting
+    document.getElementById('choose-mapping-key').style.display = 'none';
+    document.getElementById('confirm-mapping-key').style.display = 'flex';
+}
+
+// STEP 3
 
 function toggleSelectAll() {
     let checkboxes = document.querySelectorAll('.fields-to-update-checkbox');
